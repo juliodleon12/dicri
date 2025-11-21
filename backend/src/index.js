be4d9297422510f69506970fb5760cd1a9d871d2
dsjulio <julio.ds@alliedglobal.com>
2025-11-21 16:34:14 -0600

    Agregando structura de proyecto y archivos principales

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { connectDB } = require("./infrastructure/db/connection");
+
+// Rutas
+const expedienteRoutes = require("./presentation/routes/expedienteRoutes");
+const indicioRoutes = require("./presentation/routes/indicioRoutes");
+const usuarioRoutes = require("./presentation/routes/usuarioRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Conectar a SQL Server
+connectDB()
+  .then(() => console.log("SQL Server conectado"))
+  .catch(err => console.error("Error SQL", err));
+
+// Rutas
+app.use("/api/expedientes", expedienteRoutes);
+app.use("/api/indicios", indicioRoutes);
+app.use("/api/usuarios", usuarioRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`API corriendo en puerto ${PORT}`));
